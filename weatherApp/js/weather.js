b2eec96d5b77049422c1fc33e253e0b1272f13b1
unknown <mirjana312@gmail.com>
2014-10-02 14:20:39 +0200

    promena

--- weatherApp/js/weather.js
+++ weatherApp/js/weather.js
@@ -5,30 +5,32 @@ $(document).ready(function(){
 		var city = $(this).val();
 		var url = "http://api.openweathermap.org/data/2.5/weather?units=metric&q=" + city;
 
+
+
 		$.post(url, function(data){
-			/*console.log(data);*/
+			console.log(data);
+			
 			/*var city = data.name;*/
 			var temp = Math.round(data.main.temp);
 			var vremenski_uslov = data.weather[0].description;
 			var icon = data.weather[0].icon;
 			var max = Math.round(data.main.temp_max);
 			var min = Math.round(data.main.temp_min);
+			var icon_url = "http://openweathermap.org/img/w/" + icon + ".png";
 
 
 			$("#grad").text(city);
 			$("#temp").text(temp  + " °C");
 			$("#vreme").text(vremenski_uslov);
-			
-			var icon_url = "http://openweathermap.org/img/w/" + icon + ".png";
-
 			$("img.ikona").attr("src", icon_url);
 			$("#max").text(max);
 			$("#min").text(min);
 
-			
-			$('#rezultati').show();
+            $("img.ikona").show();
 
+			$('span').show();
 
+ 			
 
 		});
 
@@ -37,7 +39,7 @@ $(document).ready(function(){
 
 	});
 
-
+    
 
 	$('#car').on('click', function()
 	{
@@ -46,7 +48,7 @@ $(document).ready(function(){
 
 
 	$("input").on ("blur", function(){
-		$("#city").css({"color": "#fff", "font-size": "18px"});
+		$("#city").css({"color": "#d54d57", "font-size": "25px"});
 	});
 
 
